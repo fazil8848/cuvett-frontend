c323aa07a588fc1549cdc6e26066d79cd02925f0
fazil8848 <128672726+fazil8848@users.noreply.github.com>
2024-10-19 19:05:34 +0530

    bug fixes

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const API = axios.create({
-  baseURL: apiUrl || "http://localhost:5000/api",
+  baseURL: apiUrl || "https://cuvett-backend.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
